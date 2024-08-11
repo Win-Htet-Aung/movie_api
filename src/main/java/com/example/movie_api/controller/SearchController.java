f449f1744766699bc7b27a1224307461f9d3d7a1
Win Htet Aung <whaung1998@gmail.com>
2024-08-11 16:04:23 +0700

    fix[Search]: remove sout statement

--- src/main/java/com/example/movie_api/controller/SearchController.java
+++ src/main/java/com/example/movie_api/controller/SearchController.java
@@ -17,7 +17,6 @@ public class SearchController {
 
     @GetMapping("/search")
     public Page<MovieSeries> getSearchPage(SearchCriteria searchCriteria, Pageable pageable) {
-        System.out.println("imdbRating => " + searchCriteria.getImdbRating());
         return searchService.getSearchPage(searchCriteria, pageable);
     }
 }
